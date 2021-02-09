c4e3b33ed32a5acfc8d9bfbeddd1b31ed511cde3
Elle <lucadelc@gmail.com>
2021-02-09 09:45:27 +0100

    Centered the text inside the textbox

--- pdf-to-text.py
+++ pdf-to-text.py
@@ -30,6 +30,8 @@ def open_file():
         # Text box
         text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
         text_box.insert(1.0, page_content)
+        text_box.tag_configure("center", justify="center")
+        text_box.tag_add("center", 1.0, "end")
         text_box.grid(column=1, row=3)
 
 # Browse button
